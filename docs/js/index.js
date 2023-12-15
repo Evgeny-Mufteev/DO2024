bd711ae69241d58cf69592d3a9b5b6b8fb0e39f3
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-12-15 16:48:39 +0300

    добавил бегущую строку

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,74 +1,50 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Расчет длинны линии
-  var calcFeatureLine = function calcFeatureLine() {
-    var featureLine = document.querySelector('.feature__line');
-    var featureListHeight = document.querySelector('.feature__list').offsetHeight;
-    var featureLastItemHeight = document.querySelector('.feature__item:last-child').offsetHeight;
-    if (window.innerWidth > 1024) {
-      featureLine.style.height = featureListHeight - featureLastItemHeight / 2 + 'px';
-    } else {
-      featureLine.style.height = featureListHeight - featureLastItemHeight + 'px';
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  // Горизонтальный скролл в галериеи
+  var horizontalScrollInGallery = function horizontalScrollInGallery() {
+    var controller = new ScrollMagic.Controller();
+    var slider = document.querySelector('.gallery__slider');
+    var horizontalScrollLength = slider.scrollWidth - window.innerWidth;
+    var triggerHookValue = 170 / window.innerHeight;
+    var wipeAnimation = gsap.timeline().fromTo(slider, {
+      x: 0
+    }, {
+      x: -horizontalScrollLength,
+      ease: 'none'
+    });
+    new ScrollMagic.Scene({
+      triggerElement: '.gallery',
+      duration: horizontalScrollLength,
+      triggerHook: triggerHookValue
+    }).setPin('.gallery').setTween(wipeAnimation).addTo(controller);
   };
-  calcFeatureLine();
-  window.addEventListener("resize", calcFeatureLine);
-
-  // Авто-нумерация
-  var featureNumbering = function featureNumbering() {
-    var featureItems = document.querySelectorAll('.feature__item');
-    featureItems.forEach(function (item, index) {
-      var number = document.createElement('div');
-      number.classList.add('feature__number');
-      number.textContent = "".concat(index + 1);
-      item.appendChild(number);
+  horizontalScrollInGallery();
+  var verticalScrollSequential = function verticalScrollSequential() {
+    var controller = new ScrollMagic.Controller();
+    var bonusItems = document.querySelectorAll('.bonus-item');
+    bonusItems.forEach(function (item, index) {
+      var style = window.getComputedStyle(item);
+      var margin = parseFloat(style.marginTop) + parseFloat(style.marginBottom);
+      var padding = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+      var innerHeight = item.querySelector('.bonus-item__box').offsetHeight;
+      var fullHeight = innerHeight + padding + margin;
+      var tween = gsap.fromTo(item, {
+        height: '91px'
+      }, {
+        height: fullHeight + 'px',
+        ease: 'power1.out'
+      });
+      new ScrollMagic.Scene({
+        triggerElement: item,
+        triggerHook: 0.8,
+        duration: '100%'
+      }).setTween(tween).addTo(controller).on('end', function () {
+        if (index < bonusItems.length - 1) {
+          controller.scrollTo(bonusItems[index + 1]);
+        }
+      });
     });
   };
-  featureNumbering();
-  var reviewsSlider = new Swiper('.reviews__slider', {
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.reviews__slider-next',
-      prevEl: '.reviews__slider-prev'
-    }
-  });
-
-  // Плеер для видео
-  var composeVideo = function composeVideo() {
-    var player = document.querySelectorAll('.youtube-player');
-    var link = document.querySelector('.reviews__preview-link');
-    if (player && link) {
-      var videoId = link.innerText.split('watch?v=')[1];
-      var videoThumbnailUrl = "https://img.youtube.com/vi/".concat(videoId, "/0.jpg");
-      var videoThumbnail = document.createElement('img');
-      videoThumbnail.src = videoThumbnailUrl;
-      player.forEach(function (item) {
-        item.appendChild(videoThumbnail);
-      });
-      var loadPlayer = function loadPlayer(evt) {
-        var target = evt.currentTarget;
-        var iframe = document.createElement('iframe');
-        iframe.src = "https://www.youtube.com/embed/".concat(videoId, "?autoplay=1");
-        iframe.setAttribute('width', 100 + '%');
-        iframe.setAttribute('height', 100 + '%');
-        iframe.setAttribute('allow', 'autoplay');
-        iframe.setAttribute('loading', 'lazy');
-        target.classList.remove('btn-dis');
-        videoThumbnail.style.display = 'none';
-        if (target.children.length) {
-          target.replaceChild(iframe, target.firstElementChild);
-        } else {
-          target.appendChild(iframe);
-        }
-      };
-      var config = {
-        once: true
-      };
-      Array.from(player).forEach(function (player) {
-        player.addEventListener('click', loadPlayer, config);
-      });
-    }
-  };
-  composeVideo();
+  verticalScrollSequential();
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,76 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.gallery__swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 40,
-    freeMode: true,
-    mousewheel: true,
-  });
+  // Горизонтальный скролл в галериеи
+  const horizontalScrollInGallery = () => {
+    const controller = new ScrollMagic.Controller();
+    const slider = document.querySelector('.gallery__slider');
+    const horizontalScrollLength = slider.scrollWidth - window.innerWidth;
+    const triggerHookValue = 170 / window.innerHeight;
+    const wipeAnimation = gsap
+      .timeline()
+      .fromTo(slider, { x: 0 }, { x: -horizontalScrollLength, ease: 'none' });
+    new ScrollMagic.Scene({
+      triggerElement: '.gallery',
+      duration: horizontalScrollLength,
+      triggerHook: triggerHookValue,
+    })
+      .setPin('.gallery')
+      .setTween(wipeAnimation)
+      .addTo(controller);
+  };
+  horizontalScrollInGallery();
 
-  const isElementFullyInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= window.innerHeight &&
-      rect.right <= window.innerWidth
-    );
+  const verticalScrollSequential = () => {
+    // Создание контроллера ScrollMagic
+    var controller = new ScrollMagic.Controller();
+
+    // Настройка сцен для каждого bonus-item
+    document.querySelectorAll('.bonus-item').forEach(function (item, index) {
+      new ScrollMagic.Scene({
+        triggerElement: item,
+        triggerHook: 0.5, // настраивает положение триггера (0 - верх, 1 - низ, 0.5 - центр)
+      })
+        .on('enter', function () {
+          // Раскрывать элемент при входе в область видимости
+          item.classList.add('expanded');
+        })
+        .on('leave', function () {
+          // Скрывать элемент при выходе из области видимости
+          item.classList.remove('expanded');
+        })
+        .addTo(controller);
+    });
+  };
+  verticalScrollSequential();
+
+  // Функция для управления анимацией
+  const manageAnimation = (tickerElement, state) => {
+    const lists = tickerElement.querySelectorAll('.partners__list');
+    lists.forEach((list) => (list.style.animationPlayState = state));
   };
 
-  window.addEventListener('scroll', () => {
-    const swiperElement = document.querySelector('.gallery__swiper');
-    const topOffset = 100;
-    const bottomOffset = 100;
-    if (isElementFullyInViewport(swiperElement, topOffset, bottomOffset)) {
-      swiperElement.focus();
+  // Функция для настройки тикера
+  const setupTicker = (ticker, direction) => {
+    const originalList = ticker.querySelector('.partners__list'),
+      clone = originalList.cloneNode(true);
+
+    if (direction === '_left') {
+      ticker.appendChild(clone);
+    } else {
+      ticker.insertBefore(clone, originalList);
     }
-  });
+    // Обработчики событий для остановки и запуска анимации
+    ticker.addEventListener('mouseover', () => manageAnimation(ticker, 'paused'));
+    ticker.addEventListener('mouseout', () => manageAnimation(ticker, 'running'));
+  };
+
+  // Функция для инициализации всех тикеров
+  const setupTickers = () => {
+    document.querySelectorAll('.partners-content').forEach((ticker) => {
+      const direction = ticker.classList.contains('_left') ? '_left' : '_right';
+      setupTicker(ticker, direction);
+    });
+  };
+  setupTickers();
 });
